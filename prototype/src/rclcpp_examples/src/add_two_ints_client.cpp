b5cce49a237c439f1acc2c9026db4fea3c18451c
Dirk Thomas <dthomas@osrfoundation.org>
2014-03-26 14:23:39 -0700

    fix client node name

--- prototype/src/rclcpp_examples/src/add_two_ints_client.cpp
+++ prototype/src/rclcpp_examples/src/add_two_ints_client.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
 
-    rclcpp::Node::Ptr node = rclcpp::create_node("add_two_ints_server");
+    rclcpp::Node::Ptr node = rclcpp::create_node("add_two_ints_client");
     auto client = node->create_client<rclcpp_examples::AddTwoInts>("add_two_ints");
     rclcpp_examples::AddTwoIntsRequest req;
     req.a = 2;
