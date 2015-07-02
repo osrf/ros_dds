d14e03288a16845ccf0973dbf5af2c48161bb8a2
Jackie Kay <jacquelinekay1@gmail.com>
2015-07-02 12:18:45 -0700

    Message size parameterized, ready to take lots of data

--- realtime/dds_intraproc.cpp
+++ realtime/dds_intraproc.cpp
@@ -2,12 +2,11 @@
 #include <pthread.h>
 #include <memory>
 
-
 #include <rttest/rttest.h>
 #include "ExampleSubscriber.hpp"
-#include "UnsafePublisher.hpp"
+#include "ExamplePublisher.hpp"
 
-UnsafePublisher pub;
+ExamplePublisher pub;
 ExampleSubscriber sub;
 
 static void start_rt_thread(void *(*f)(void*))
@@ -61,6 +60,8 @@ void *subscriber_thread(void *unused)
 
 	rttest_init_new_thread();
 	rttest_set_sched_priority(90, SCHED_RR);
+	rttest_lock_memory();
+	rttest_prefault_stack_size(4096);
 
 	rttest_spin(sub_callback, NULL);
 
@@ -71,7 +72,39 @@ void *subscriber_thread(void *unused)
 
 int main(int argc, char *argv[])
 {
+  int c;
+
+  unsigned int message_length;
+  // l stands for message length
+  opterr = 0;
+  optind = 1;
+  int argc_copy = argc;
+  char *argv_copy[argc];
+  for (int i = 0; i < argc; ++i)
+  {
+    size_t len = strlen(argv[i]);
+    argv_copy[i] = (char*) malloc(len);
+    memcpy(argv_copy[i], argv[i], len);
+  }
+
+  while ((c = getopt(argc_copy, argv_copy, "l:")) != -1)
+  {
+    switch(c)
+    {
+      case 'l':
+        message_length = std::stoul(std::string(optarg));
+        break;
+      default:
+        break;
+    }
+  }
 	rttest_read_args(argc, argv);
+
+  pub.message_size = message_length;
+	rttest_read_args(argc, argv);
+
+	rttest_lock_memory();
+	rttest_prefault_stack_size(message_length + 4096);
   start_rt_thread(&subscriber_thread);
 	publisher_thread(NULL);
 }
