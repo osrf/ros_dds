be1ff04324080f9577cdc529f86cf49ab6c19d1d
Jackie Kay <jackie@osrfoundation.org>
2015-07-06 16:27:18 -0700

    C examples

---
+++ realtime_c/rt_publisher.cpp
@@ -0,0 +1,29 @@
+#include <rttest/rttest.h>
+#include "ExamplePublisher.hpp"
+
+#define STACK_SIZE 1024*1024*1024
+
+ExamplePublisher pub;
+
+void* pub_callback(void * unused)
+{
+	pub.callback();
+}
+
+int main(int argc, char *argv[])
+{
+	pub.init();
+
+	rttest_read_args(argc, argv);
+	rttest_set_sched_priority(90, SCHED_RR);
+	//rttest_lock_memory();
+	//rttest_prefault_stack_size(STACK_SIZE);
+	//rttest_lock_and_prefault_dynamic(STACK_SIZE);
+
+	rttest_spin(pub_callback, NULL);
+
+	rttest_write_results();
+	rttest_finish();
+
+	pub.teardown();
+}
---
+++ realtime_c/rt_subscriber.cpp
@@ -0,0 +1,29 @@
+#include <rttest/rttest.h>
+#include "ExampleSubscriber.hpp"
+
+#define STACK_SIZE 1024*1024*1024
+
+ExampleSubscriber sub;
+
+void* sub_callback(void * unused)
+{
+	sub.callback();
+}
+
+int main(int argc, char *argv[])
+{
+	sub.init();
+
+	rttest_read_args(argc, argv);
+	rttest_set_sched_priority(90, SCHED_RR);
+	//rttest_lock_memory();
+	//rttest_prefault_stack_size(STACK_SIZE);
+	rttest_lock_and_prefault_dynamic(STACK_SIZE);
+
+	rttest_spin(sub_callback, NULL);
+
+	rttest_write_results();
+	rttest_finish();
+
+	sub.teardown();
+}
