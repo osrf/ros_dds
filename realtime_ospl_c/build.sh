b09eb99e7f84b112efdb6863acce3282af4bb0a2
Jackie Kay <jackie@osrfoundation.org>
2015-07-10 10:52:07 -0700

    Improve build script

--- realtime_ospl_c/build.sh
+++ realtime_ospl_c/build.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env sh
+
+# This script generates message code from the IDL file, creates a CMake build
+# directory and invokes CMake to build the examples.
+# Arguments to this script are passed directly to cmake, e.g.
+# $ . build.sh -DCMAKE_BUILD_TYPE=Release
+# will build the examples in release mode.
+
 rm -fr c
 mkdir c
 cd c
@@ -8,6 +16,6 @@ cd ..
 rm -fr build
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=~/local
+cmake .. $@
 make
 cd ..
